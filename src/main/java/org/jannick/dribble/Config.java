31f9ab4472ceb051e94842c5fbe31a457d6b0831
Jannick <63607585+Jannick05@users.noreply.github.com>
2022-04-06 22:47:31 +0200

    Update Config.java

--- src/main/java/org/jannick/dribble/Config.java
+++ src/main/java/org/jannick/dribble/Config.java
@@ -1,10 +1,7 @@
 package org.jannick.dribble;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class Config {
     public static String getToken() {
-        Dotenv dotenv = Dotenv.load();
-        return dotenv.get("TOKEN");
+        return System.getenv("TOKEN");
     }
 }
